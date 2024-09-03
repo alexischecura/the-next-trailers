6c014bb6c12132562c760f7baff18822aa91f7d5
Alexis Checura <alexischecura@gmail.com>
2024-09-03 15:07:56 -0300

    refactor: makes the logo a link to home

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -1,13 +1,15 @@
+import Link from 'next/link';
+
 function Logo() {
   return (
-    <>
+    <Link href="/">
       <h2 className="flex flex-col text-white uppercase">
-        <span className="text-xl">Next</span>
+        <span className="text-xl">The Next</span>
         <span className="font-bold text-3xl uppercase leading-4 tracking-tight">
           Trailers
         </span>
       </h2>
-    </>
+    </Link>
   );
 }
 
