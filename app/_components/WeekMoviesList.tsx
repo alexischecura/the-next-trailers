e36f616a55832f8a37afdaa627ecc2e2d2fe1a2d
Alexis Checura <alexischecura@gmail.com>
2024-09-19 17:49:27 -0300

    feat: add week movies list

--- app/_components/WeekMoviesList.tsx
+++ app/_components/WeekMoviesList.tsx
@@ -1,28 +1,16 @@
-import Image from 'next/image';
-import { mainMovies } from '../data/movies';
+import { getPopularWeekMovies } from '../_lib/data-service';
+import MoviesGrid from './MoviesGrid';
 
-function WeekMoviesList() {
+async function WeekMoviesList() {
+  const popularWeekMovies = await getPopularWeekMovies();
+
+  if (!popularWeekMovies) return null;
+
   return (
     <section className="bg-blue-950 pt-20 text-white">
       <div className="mx-auto max-w-7xl">
-        <h3 className='mb-6 font-semibold text-xl'>🔥 TRENDING THIS WEEK</h3>
-        <div className="flex justify-between">
-          {mainMovies.map((movie) => {
-            return (
-              <div key={movie.imdbID}>
-                <Image
-                  src={movie.Poster}
-                  alt={movie.Title}
-                  height="300"
-                  width="200"
-                  className="rounded-2xl"
-                />
-                <h5>{movie.Title}</h5>
-                <p>{movie.Released}</p>
-              </div>
-            );
-          })}
-        </div>
+        <h3 className="mb-6 font-semibold text-xl">🔥 TRENDING THIS WEEK</h3>
+        <MoviesGrid movies={popularWeekMovies} quantity={10} />
       </div>
     </section>
   );
